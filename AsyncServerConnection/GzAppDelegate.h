dda2782d661e39808b48e621c6dd2995ee077cdb
Sebastiao Gazolla jr <gazolla@gmail.com>
2014-08-01 09:28:39 -0300

    Initial Commit

---
+++ AsyncServerConnection/GzAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GzAppDelegate.h
+//  AsyncServerConnection
+//
+//  Created by Gazolla on 01/08/14.
+//  Copyright (c) 2014 Gazolla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GzAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AsyncServerConnectionTests/AsyncServerConnectionTests.m
@@ -0,0 +1,34 @@
+//
+//  AsyncServerConnectionTests.m
+//  AsyncServerConnectionTests
+//
+//  Created by Gazolla on 01/08/14.
+//  Copyright (c) 2014 Gazolla. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AsyncServerConnectionTests : XCTestCase
+
+@end
+
+@implementation AsyncServerConnectionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
